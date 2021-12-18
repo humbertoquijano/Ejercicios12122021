cd49b4ec77e1a6ff1371847fe2a852530dfbcfea
Humberto Quijano <humberto.quijano@gmail.com>
2021-12-18 15:39:26 -0500

    Add project files.

---
+++ Ejercicios12122021/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios12122021
+{
+    public class Item
+    {
+        public DateTime FechaRegistro { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
+
---
+++ Ejercicios12122021/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Ejercicios12122021;
+using Newtonsoft.Json;
+using System;
+
+string ruta = @"D:\mtiexe\Natalia\Datos.csv"; 
+String [] datos=File.ReadAllLines(ruta);
+foreach (String dato in datos)
+{ 
+    Console.WriteLine(dato);
+    Item item=BuildItem(dato);
+    Console.WriteLine(JsonConvert.SerializeObject(item));
+}
+
+static Item BuildItem(String dato)
+{
+    try
+    {
+        String[] valores = dato.Split(';');
+        return new Item { Cantidad = Convert.ToInt32(valores[1]), FechaRegistro = Convert.ToDateTime(valores[0]) };
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+        return new Item { Cantidad = -1, FechaRegistro = DateTime.MinValue };
+    }    
+}
